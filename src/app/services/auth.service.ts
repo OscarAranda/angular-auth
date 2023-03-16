f9c197bd8aa3a844c89c66f188ebf9cbebce8a86
Oscar Aranda <oscararanda1984@gmail.com>
2023-03-16 14:15:40 -0300

    se creo el registro y el recovery de cuantes.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { environment } from '@environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = environment.API_URL;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(email: string, password: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`,{ email, password});
+  }
+
+  register(name: string, email: string, password: string ){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/register`,{ name, email, password});
+  }
+
+  registerAndLogin(){
+
+  }
+
+  isAvailable(email: string){
+    return this.http.post<{isAvailable : boolean}>(`${this.apiUrl}/api/v1/auth/is-available`,{ email });
+
+  }
+
+  recovery( email: string ){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/recovery`,{ email });
+
+  }
+
+  changePassword( token: string, newPassword: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/change-password`,{ token, newPassword });
+
+  }
+
+
+
+}
